6df6773548cb7325a3aff53be08a39a258a13dcf
Sérgio Moura <smouracalmon@gmail.com>
2022-03-29 06:12:25 -0300

    Alterada estrutura para que histórico guarde o http code da imagem associada

--- main.js
+++ main.js
@@ -9,49 +9,50 @@ form.addEventListener('submit', onFormSubmit);
 
 async function onFormSubmit(evt) {
     evt.preventDefault();
-    let id = field.value;
+    let code = field.value;
     try {
-        let image = await getImage(id);
-        show(image);
-        history(image);
+        let statusImage = await getStatusImage(code);
+        show(statusImage);
+        history(statusImage);
     } catch (error) {
         alert(error)
     }
 }
 
-async function getImage(id){
-    let response = await fetch(`${baseUrl}/${id}.jpg`);
+async function getStatusImage(code){
+    let response = await fetch(`${baseUrl}/${code}.jpg`);
     if(response.status == 200){
         let img = await response.blob();
-        return img;
+        return {code, img};
     } else {
         throw("Failed")
     }
 }
 
-function show(image){
+function show(statusImage){
 
     let reader = new FileReader();
-    reader.readAsDataURL(image);
+    reader.readAsDataURL(statusImage.img);
 
     reader.onload = function() {
         imgContainer.src = `data:${reader.result}`
+        field.value = statusImage.code;
     };
 
 }
 
-function history(image) {
-    imageHistory.push(image);
+function history(statusImage) {
+    imageHistory.push(statusImage);
     
     let img = document.createElement('img');
 
     let reader = new FileReader();
-    reader.readAsDataURL(image);
+    reader.readAsDataURL(statusImage.img);
 
     reader.onload = function() {
         
         img.src = `data:${reader.result}`
-        img.addEventListener('click',()=>{show(image)});
+        img.addEventListener('click',()=>{show(statusImage)});
 
         let paragraf = document.querySelector('#history > p');
         if(paragraf !== null){
